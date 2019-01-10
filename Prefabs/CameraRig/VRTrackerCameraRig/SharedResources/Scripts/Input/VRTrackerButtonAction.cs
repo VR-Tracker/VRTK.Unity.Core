bb7a6049e125b16b465d28cdd4b4a6652b4f6c3c
Unknown <mat.pay@Hotmail.ca>
2019-01-10 15:01:06 -0500

    Fix VR Tracker Button system

--- Prefabs/CameraRig/VRTrackerCameraRig/SharedResources/Scripts/Input/VRTrackerButtonAction.cs
+++ Prefabs/CameraRig/VRTrackerCameraRig/SharedResources/Scripts/Input/VRTrackerButtonAction.cs
@@ -9,32 +9,25 @@
     public class VRTrackerButtonAction : BooleanAction
     {
         public enum ButtonType {Trigger, Grab, A, B, X, Y, Joystick};
-
-        public VRTracker.Manager.VRT_Tag.TagType controllerType;
+        public VRTracker.Manager.VRT_Tag tracker;
         public ButtonType buttonType;
-
-        private VRTracker.Manager.VRT_Tag tag;
-
 
 		private void Start()
 		{
-            tag = VRTracker.Manager.VRT_Manager.Instance.GetTag(controllerType);
 		}
 
 		protected virtual void Update()
         {
-            if(tag == null)
+            if (tracker != null)
             {
-                tag = VRTracker.Manager.VRT_Manager.Instance.GetTag(controllerType);
-                if (tag == null)
-                    return; // Could'nt access the controller 
+                if (buttonType == ButtonType.Trigger)
+                {
+                    Receive(tracker.trigger);
+                }
+
+                else if (buttonType == ButtonType.Grab)
+                    Receive(tracker.grab);
             }
-
-            if(buttonType == ButtonType.Trigger)
-                Receive(tag.trigger);
-
-            else if (buttonType == ButtonType.Grab)
-                Receive(tag.grab);
         }
     }
 }
